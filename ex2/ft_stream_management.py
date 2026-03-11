3bfb97ad22c31499dc03695636c2e5a8aa0d38b3
finorak <finoana265@gmail.com>
2026-03-11 20:39:12 +0300

    strip function done

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -1,11 +1,20 @@
 import sys
 
 
+def ft_strip(string: str) -> str:
+    strings = ""
+    for char in string:
+        if char == "\n":
+            break
+        strings += char
+    return strings
+
+
 def main() -> None:
     print("\nInput Stream active. Enter archivist ID: ", end="", flush=True)
-    id_ = sys.stdin.readline().strip()
+    id_ = ft_strip(sys.stdin.readline())
     print("input Stream active. Enter status report: ", end="", flush=True)
-    report = sys.stdin.readline().strip()
+    report = ft_strip(sys.stdin.readline())
     sys.stdout.write(f"\n[STANDARD] Archive status from {id_}: ")
     sys.stdout.write(f"All systems norminal: {report}\n")
     comm = "Communication channels verified"
